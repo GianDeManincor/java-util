803db00978e149f35920745e4808080e5206894e
Gian de Manincor <gianmanincor@gmail.com>
2023-12-28 19:32:35 -0300

    feat: override

---
+++ src/com/github/giandemanincor/javacore/Ioverride/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package com.github.giandemanincor.javacore.Ioverride.test;
+
+import com.github.giandemanincor.javacore.Ioverride.domain.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Strange");
+        System.out.println(anime);
+    }
+}
